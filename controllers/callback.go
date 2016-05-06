7309bd33c888b1b55d8db1856d82f4dadb60bbe6
Jesse Millar <hellojessemillar@gmail.com>
2016-05-06 07:20:40 -0600

    Logging in works, just need to figure out sessions now

--- controllers/callback.go
+++ controllers/callback.go
@@ -4,30 +4,22 @@ import (
 	// Don't forget this first import or nothing will work
 	_ "crypto/sha512"
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"net/http"
 	"os"
 
-	"github.com/jessemillar/byudzhet/packages/app"
 	"github.com/labstack/echo"
 	"golang.org/x/oauth2"
 )
 
 func CallbackHandler(c echo.Context) error {
-	fmt.Printf("%+v\n", c)
-
-	return c.String(http.StatusOK, "DONE")
-}
-
-func CallbackHandler2(w http.ResponseWriter, r *http.Request) {
 	domain := "jessemillar.auth0.com"
 
 	// Instantiating the OAuth2 package to exchange the Code for a Token
 	conf := &oauth2.Config{
 		ClientID:     os.Getenv("AUTH0_CLIENT_ID"),
 		ClientSecret: os.Getenv("AUTH0_CLIENT_SECRET"),
-		RedirectURL:  os.Getenv("AUTH0_CALLBACK_URL"),
+		RedirectURL:  "http://localhost:8000/callback",
 		Scopes:       []string{"openid", "name", "email", "nickname"},
 		Endpoint: oauth2.Endpoint{
 			AuthURL:  "https://" + domain + "/authorize",
@@ -36,49 +28,47 @@ func CallbackHandler2(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// Getting the Code that we got from Auth0
-	code := r.URL.Query().Get("code")
+	code := c.QueryParam("code")
 
 	// Exchanging the code for a token
 	token, err := conf.Exchange(oauth2.NoContext, code)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		return c.String(http.StatusInternalServerError, err.Error())
 	}
 
 	// Getting now the User information
 	client := conf.Client(oauth2.NoContext, token)
 	resp, err := client.Get("https://" + domain + "/userinfo")
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		return c.String(http.StatusInternalServerError, err.Error())
 	}
 
 	// Reading the body
 	raw, err := ioutil.ReadAll(resp.Body)
 	defer resp.Body.Close()
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		return c.String(http.StatusInternalServerError, err.Error())
 	}
 
 	// Unmarshalling the JSON of the Profile
 	var profile map[string]interface{}
 	if err := json.Unmarshal(raw, &profile); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		return c.String(http.StatusInternalServerError, err.Error())
 	}
 
 	// Saving the information to the session.
 	// We're using https://github.com/astaxie/beego/tree/master/session
 	// The GlobalSessions variable is initialized in another file
 	// Check https://github.com/auth0/auth0-golang/blob/master/examples/regular-web-app/app/app.go
-	session, _ := app.GlobalSessions.SessionStart(w, r)
-	defer session.SessionRelease(w)
+	// session, _ := app.GlobalSessions.SessionStart(c, c.Request())
+	// defer session.SessionRelease(c)
 
-	session.Set("id_token", token.Extra("id_token"))
-	session.Set("access_token", token.AccessToken)
-	session.Set("profile", profile)
+	// session.Set("id_token", token.Extra("id_token"))
+	// session.Set("access_token", token.AccessToken)
+	// session.Set("profile", profile)
 
 	// Redirect to logged in page
-	http.Redirect(w, r, "/user", http.StatusMovedPermanently)
+	c.Redirect(http.StatusMovedPermanently, "/health")
+
+	return c.String(http.StatusOK, "DONE")
 }
