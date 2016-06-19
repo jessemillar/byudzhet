1aa3dd8196afaccc9fa1809f87d4dfccaee3b2d0
Jesse Millar <hellojessemillar@gmail.com>
2016-06-19 12:49:11 -0600

    Trying something different to make ontouchstart work

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -3,18 +3,6 @@ var selectedBucket; // A global for keeping track of which bucket is selected in
 var projected = false;
 var currentTab = "";
 
-(function(a, b, c) { // Make the app work as a single-page app on iOS devices
-    if (c in b && b[c]) {
-        var d, e = a.location,
-            f = /^(a|html)$/i;
-        a.addEventListener("click", function(a) {
-            d = a.target;
-            while (!f.test(d.nodeName)) d = d.parentNode;
-            "href" in d && (chref = d.href).replace(e.href, "").indexOf("#") && (!/^[a-z\+\.\-]+:/i.test(chref) || chref.indexOf(e.protocol + "//" + e.host) === 0) && (a.preventDefault(), e.href = d.href)
-        }, !1);
-    }
-})(document, window.navigator, "standalone");
-
 $(function() {
     $("body").on('click', '.dropdown-menu li a', function() { // Populate #bucket-dropdown with selected item
         selectedBucket = $(this).text();
